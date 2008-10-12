6683012f7e7ce834e85736b6a8dabc32c4677c81
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-10-12 17:10:30 +0000

    Added handling for multiple connections in "db" application aspect

--- library/Kajoa/Application/Aspect/Db.php
+++ library/Kajoa/Application/Aspect/Db.php
@@ -5,34 +5,55 @@ require_once 'Zend/Db/Table/Abstract.php';
 
 class Kajoa_Application_Aspect_Db extends Kajoa_Application_Aspect_Abstract
 {
+    protected $_connections = array();
+
     protected $_defaultSettings = array(
-        'production'  => array(
-            'connectionCharset' => '',
-            'profilerClass'     => '', 
-        ),
+        'production'  => array(),
         'testing'     => array(),
-        'development' => array(
-            'profilerClass'     => 'Zend_Db_Profiler_Firebug', 
-        ),
+        'development' => array(),
     );
     
-    public function init()
+    protected function _loadConnection($name, Zend_Config $settings)
     {
-        $settings = $this->getSettings();
-
-        $db = Zend_Db::factory($settings->adapter, $settings);
+        $connection = Zend_Db::factory($settings->adapter, $settings);
         
         if (!empty($settings->profilerClass)) {
             Zend_Loader::loadClass($settings->profilerClass);
             $profiler = new $settings->profilerClass('Database queries');
             $profiler->setEnabled(true);
-            $db->setProfiler($profiler);
+            $connection->setProfiler($profiler);
         }
         
         if (!empty($settings->connectionCharset)) {
-            $db->query("SET NAMES '$settings->connectionCharset'");
+            $connection->query("SET NAMES '$settings->connectionCharset'");
         }
         
-        Zend_Db_Table_Abstract::setDefaultAdapter($db);
+        if ($settings->isDefault) {
+            Zend_Db_Table_Abstract::setDefaultAdapter($connection);            
+        }
+        
+        $this->_connections[$name] = $connection;
+    }
+    
+    public function getConnection($name = 'default')
+    {
+        if (!array_key_exists($name, $this->_connections)) {
+            throw new Kajoa_Exception("Unknown connection ($name)");
+        }
+        
+        return $this->_connections[$name];
+    }
+    
+    public function init()
+    {
+        $settings = $this->getSettings();
+
+        if (null == $settings->adapter) {
+            foreach ($settings as $connectionName => $connectionSettings) {
+                $this->_loadConnection($connectionName, $connectionSettings);
+            }
+        } else {
+            $this->_loadConnection('default', $settings);
+        }
     }
 }
