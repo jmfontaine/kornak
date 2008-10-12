c78da2a2cae284b20a6bbeeebaf8095cb746f810
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-10-12 17:40:05 +0000

    Added shortcut to access application instance in controllers

--- library/Kajoa/Controller/Action.php
+++ library/Kajoa/Controller/Action.php
@@ -1,11 +1,17 @@
 <?php
+require_once 'Kajoa/Application.php';
+
 class Kajoa_Controller_Action extends Zend_Controller_Action
 {
+    protected $_application;
+    
     public function init()
     {
         parent::init();
         
         $moduleName = $this->getRequest()->getModuleName();
         $this->_helper->layout->setLayout($moduleName . '/views/layouts/main');
+        
+        $this->_application = Kajoa_Application::getInstance();
     }
 }
