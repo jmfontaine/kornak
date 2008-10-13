b4a819ba92ff3df0b9e36b1fea3328e9b6c7b79f
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-10-13 10:45:35 +0000

    Added type hinting for methods Kajoa_Model_Abstract::_createItem() and Kajoa_Model_Abstract::_createItemset()

--- library/Kajoa/Model/Abstract.php
+++ library/Kajoa/Model/Abstract.php
@@ -7,14 +7,14 @@ abstract class Kajoa_Model_Abstract
     protected $_itemClass    = 'Kajoa_Model_Item';
     protected $_itemsetClass = 'Kajoa_Model_Itemset';
     
-    protected function _createItem($data)
+    protected function _createItem(array $data)
     {
         $config = array('data' => $data);
         Kajoa_Loader::loadClass($this->_itemClass);
         return new $this->_itemClass($config);
     }
     
-    protected function _createItemset($data)
+    protected function _createItemset(array $data)
     {
         $config = array('data' => $data);
         Kajoa_Loader::loadClass($this->_itemsetClass);
