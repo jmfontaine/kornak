d0469816e42dcb9afef4e42f74dacfc8bb2d537f
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-10-19 20:47:08 +0000

    Added Kajoa_Loader::loadForm() method

--- library/Kajoa/Loader.php
+++ library/Kajoa/Loader.php
@@ -3,6 +3,19 @@ require_once "Zend/Loader.php";
 
 class Kajoa_Loader extends Zend_Loader
 {
+    public static function loadForm($name, $module = null)
+    {
+        $frontController = Zend_Controller_Front::getInstance();
+        if (null === $module) {
+            $module = $frontController->getDefaultModule();            
+        }
+        
+        $applicationPath = Kajoa_Application::getInstance()->getApplicationPath(); 
+        $formPath        = $applicationPath . '/modules/' . $module . '/forms';  
+        
+        self::loadClass($name, $formPath);
+    }
+    
     public static function loadModel($name, $module = null)
     {
         $frontController = Zend_Controller_Front::getInstance();
