2eb29b280b3bf128e4835ca4f4d839dad584937b
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-10-23 14:56:15 +0000

    Added "Model" view helper

---
+++ library/Kajoa/View/Helper/Model.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'Kajoa/Loader.php';
+
+class Kajoa_View_Helper_Model extends Zend_View_Helper_Abstract
+{
+    public function model($name, $module = null)
+    {
+        Kajoa_Loader::loadModel($name, $module);
+        return new $name();
+    }
+}
