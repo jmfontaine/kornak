160b8c0017b34e6f06376f916c2ab26dcdb81d90
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-10-29 14:08:55 +0000

    Added a custom "Label" form decorator

---
+++ library/Kajoa/Form/Decorator/Label.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'Kajoa/Form/Decorator/Label.php';
+
+class Kajoa_Form_Decorator_Label extends Zend_Form_Decorator_Label
+{
+    public function render($content)
+    {
+        $element = $this->getElement();
+        $view    = $element->getView();
+        if (null === $view) {
+            return $content;
+        }
+
+        $label     = $this->getLabel();
+        $separator = $this->getSeparator();
+        $placement = $this->getPlacement();
+        $tag       = $this->getTag();
+        $id        = $this->getId();
+        $class     = $this->getClass();
+        $options   = $this->getOptions();
+        $messages  = $this->getElement()->getMessages();
+        
+        if (empty($label) && empty($tag) && empty($messages)) {
+            return $content;
+        }
+
+        $includeErrors = !empty($options['includeErrors']);
+        if ($includeErrors && !empty($messages)) {
+            unset($options['includeErrors']);
+            require_once 'Zend/Form/Decorator/HtmlTag.php';
+            $decorator = new Zend_Form_Decorator_HtmlTag();
+            $decorator->setOptions(array('tag' => 'strong'));
+            $label .= $decorator->render(current($messages));
+        }
+        
+        if (!empty($label)) {
+            $options['class']  = $class;
+            $label = $view->formLabel($element->getFullyQualifiedName(), trim($label), $options);
+        } else {
+            $label = '&nbsp;';
+        }
+        
+        if (null !== $tag) {
+            require_once 'Zend/Form/Decorator/HtmlTag.php';
+            $decorator = new Zend_Form_Decorator_HtmlTag();
+            $decorator->setOptions(array('tag' => $tag));
+            $label = $decorator->render($label);
+        }
+
+        switch ($placement) {
+            case self::APPEND:
+                return $content . $separator . $label;
+            case self::PREPEND:
+                return $label . $separator . $content;
+        }
+    }
+}
