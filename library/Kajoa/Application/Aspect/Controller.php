fcbb77501a6e37f8a5b0e2dad8723a640934fae5
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-11-18 10:23:11 +0000

    Changed the way routes are loaded to use Zend Framework 1.7.0 improvements

--- library/Kajoa/Application/Aspect/Controller.php
+++ library/Kajoa/Application/Aspect/Controller.php
@@ -6,16 +6,16 @@ class Kajoa_Application_Aspect_Controller extends Kajoa_Application_Aspect_Abstr
 {
     protected $_defaultSettings = array(
         'production'  => array(
-            'routesUseLocales' => false,
-            'routesFilePath'   => 'config/routes.ini',
-            'throwException'   => false,
+            'mapHostnameToLocale' => false,
+            'routesFilePath'      => 'config/routes.ini',
+            'throwException'      => false,
         ),
         'testing'     => array(),
         'development' => array(
             'throwException' => true,
         ),
     );
-    
+
     protected function _loadRoutes()
     {
         $frontController = Zend_Controller_Front::getInstance();
@@ -23,57 +23,40 @@ class Kajoa_Application_Aspect_Controller extends Kajoa_Application_Aspect_Abstr
         // KLUDGE: Workaround a Zend Framework bug
         $request = new Zend_Controller_Request_Http();
         $frontController->setRequest($request);
-        
+
         $frontController->throwExceptions($this->getSetting('throwException'));
-        
+
         $router = $frontController->getRouter();
-        
+
         $applicationPath = $this->getApplication()->getApplicationPath();
         $routesFilePath  = $applicationPath . '/' . $this->getSetting('routesFilePath');
         $routesConfig    = new Zend_Config_Ini($routesFilePath, null, true);
 
-        if (true == $this->getSetting('routesUseLocales')) {
-            foreach ($routesConfig as $locale => $routes) {
-                // Handle hostname if present
-                $hostnameRoute = null;
-                if (isset($routes->hostname)) {
-                    $hostnameRoute = new Zend_Controller_Router_Route_Hostname(
-                        $routes->hostname->route,
-                        array('locale' => $locale)
-                    );
-                    unset($routes->hostname);
-                }
-                
-                foreach ($routes as $name => $values) {
-                    $class = (isset($values->type)) ? $values->type : 'Zend_Controller_Router_Route';
-                    Zend_Loader::loadClass($class);
-    
-                    $route = call_user_func(array($class, 'getInstance'), $values);
-                    
-                    if (null === $hostnameRoute) {
-                        $router->addRoute($name . '-' . $locale, $route);
-                    } else {
-                        $router->addRoute($name . '-' . $locale, $hostnameRoute->chain($route));
-                    }
+        // Map hostname to locale if needed
+        if (true == $this->getSetting('mapHostnameToLocale')) {
+            foreach ($routesConfig as $locale => $hostname) {
+                foreach ($hostname->chains as $route) {
+                    $defaults = $route->defaults;
+                    $defaults->locale = $locale;
                 }
             }
-        } else {
-            $router->addConfig($routesConfig);
         }
+
+        $router->addConfig($routesConfig);
     }
-    
+
     public function init()
     {
         $applicationPath = $this->getApplication()->getApplicationPath();
-        
+
         $this->_loadRoutes();
-        
+
         $frontController = Zend_Controller_Front::getInstance();
         $frontController->addModuleDirectory($applicationPath . '/modules');
-    }    
+    }
 
     public function run()
     {
         Zend_Controller_Front::getInstance()->dispatch();
-    }    
+    }
 }
