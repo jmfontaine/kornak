07dd33a47277d9b18db7218ba58f5a5197fc4658
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-11-21 14:07:33 +0000

    Fixed a bug in "Truncate" view helper

--- library/Kajoa/View/Helper/Truncate.php
+++ library/Kajoa/View/Helper/Truncate.php
@@ -5,6 +5,10 @@ class Kajoa_View_Helper_Truncate extends Zend_View_Helper_Abstract
 {
     public function truncate($text, $length = 30)
     {
-        return substr($text, 0, $length) . '…';
+        if (strlen($text) > $length) {
+            $text = substr($text, 0, $length) . '…';
+        }
+
+        return $text;
     }
 }
