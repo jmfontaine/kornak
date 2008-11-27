1bd23dfb93fe32f3d8ef5d56b2150569c1ad56d7
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-11-27 17:09:18 +0000

    _createItem() method of models now return false when there is no data for the item

--- library/Kajoa/Model/Abstract.php
+++ library/Kajoa/Model/Abstract.php
@@ -6,26 +6,30 @@ abstract class Kajoa_Model_Abstract
     protected $_adapters     = array();
     protected $_itemClass    = 'Kajoa_Model_Item';
     protected $_itemsetClass = 'Kajoa_Model_Itemset';
-    
+
     protected function _createItem($data)
     {
+        if (empty($data)) {
+            return false;
+        }
+
         $config = array('data' => $data);
         Kajoa_Loader::loadClass($this->_itemClass);
         return new $this->_itemClass($this, $config);
     }
-    
+
     protected function _createItemset($data)
     {
         $config = array('data' => $data);
         Kajoa_Loader::loadClass($this->_itemsetClass);
         return new $this->_itemsetClass($this, $config);
     }
-    
+
     public function __construct()
     {
         $this->init();
     }
-    
+
     public function getAdapter($name = 'default')
     {
         if (array_key_exists($name, $this->_adapters)) {
@@ -34,11 +38,11 @@ abstract class Kajoa_Model_Abstract
             return false;
         }
     }
-    
+
     public function init()
     {
     }
-    
+
     public function setAdapter($adapter, $name = 'default')
     {
         $this->_adapters[$name] = $adapter;
