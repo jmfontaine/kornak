48b104953513370bf5bafe54033c42d2229566fb
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-12-16 12:37:59 +0000

    Added $autoConvertToRelative argument to "url" view helper

--- library/Kajoa/View/Helper/Url.php
+++ library/Kajoa/View/Helper/Url.php
@@ -3,16 +3,16 @@ require_once 'Zend/View/Helper/Url.php';
 
 class Kajoa_View_Helper_Url extends Zend_View_Helper_Url
 {
-    public function url(array $urlOptions = array(), $name = null, $reset = false, $encode = true)
+    public function url(array $urlOptions = array(), $name = null, $reset = false, $encode = true, $autoConvertToRelative = true)
     {
         $url = parent::url($urlOptions, $name, $reset, $encode);
 
         // Convert to relative URL if hostname is the same as current one
-        if ($_SERVER['HTTP_HOST'] == parse_url($url, PHP_URL_HOST)) {
+        if ($autoConvertToRelative && $_SERVER['HTTP_HOST'] == parse_url($url, PHP_URL_HOST)) {
             $position = strpos($url, $_SERVER['HTTP_HOST']) + strlen($_SERVER['HTTP_HOST']);
-            $url = substr($url, $position);            
+            $url = substr($url, $position);
         }
-        
+
         return $url;
     }
 }
