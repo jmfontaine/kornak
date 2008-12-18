bf4813d0fb896c12c8e2b07098d6ded9e5ec0b2c
Jean-Marc Fontaine <jm@jmfontaine.net>
2008-12-18 16:13:10 +0000

    Modified and enhanced the way wrapping placement is handled

--- library/Kajoa/Form/Decorator/Label.php
+++ library/Kajoa/Form/Decorator/Label.php
@@ -3,6 +3,32 @@ require_once 'Kajoa/Form/Decorator/Label.php';
 
 class Kajoa_Form_Decorator_Label extends Zend_Form_Decorator_Label
 {
+    const WRAP_BEFORE = 'WRAP_BEFORE';
+    const WRAP_AFTER  = 'WRAP_AFTER';
+
+    public function getPlacement()
+    {
+        if (null !== ($placementOpt = $this->getOption('placement'))) {
+            $placementOpt = strtoupper($placementOpt);
+            switch ($placementOpt) {
+                case self::APPEND:
+                case self::PREPEND:
+                case self::WRAP_BEFORE:
+                case self::WRAP_AFTER:
+                    $this->_placement = $placementOpt;
+                    break;
+                case false:
+                    $this->_placement = null;
+                    break;
+                default:
+                    break;
+            }
+            $this->removeOption('placement');
+        }
+
+        return $this->_placement;
+    }
+
     public function render($content)
     {
         $element = $this->getElement();
@@ -41,7 +67,7 @@ class Kajoa_Form_Decorator_Label extends Zend_Form_Decorator_Label
             $label = '&nbsp;';
         }
 
-        if (null !== $tag && '' != $placement) {
+        if (null !== $tag && self::WRAP_BEFORE != $placement && self::WRAP_AFTER != $placement) {
             require_once 'Zend/Form/Decorator/HtmlTag.php';
             $decorator = new Zend_Form_Decorator_HtmlTag();
             $decorator->setOptions(array('tag' => $tag));
@@ -53,9 +79,17 @@ class Kajoa_Form_Decorator_Label extends Zend_Form_Decorator_Label
                 return $content . $separator . $label;
             case self::PREPEND:
                 return $label . $separator . $content;
-            default:
-                $label = substr($label, 0, -8);
-                return $label . $content . '</label>';
+            case self::WRAP_BEFORE:
+            case self::WRAP_AFTER:
+                $position   = strpos($label, '>');
+                $openingTag = substr($label, 0, $position + 1);
+                $text       = substr($label, $position + 1, -8);
+
+                if (self::WRAP_BEFORE == $placement) {
+                    return $openingTag . $text . $content . '</label>';
+                } else {
+                    return $openingTag . $content . $text . '</label>';
+                }
         }
     }
 }
