84c1b6743333dfcffd04feab59e02e0cc9bb30a5
Jean-Marc Fontaine <jm@jmfontaine.net>
2009-02-11 10:59:40 +0000

    Updated Kajoa version number and added Kajoa_Version::compareVersion() method

--- library/Kajoa/Version.php
+++ library/Kajoa/Version.php
@@ -1,5 +1,20 @@
 <?php
-class Kajoa_Version
+final class Kajoa_Version
 {
-    const VERSION = '0.1';
+    const VERSION = '0.2dev';
+
+    /**
+     * Compare the specified Kajoa version string $version
+     * with the current Kajoa_Version::VERSION of Kajoa.
+     *
+     * @param  string  $version  A version string (e.g. "0.7.1").
+     * @return boolean           -1 if the $version is older,
+     *                           0 if they are the same,
+     *                           and +1 if $version is newer.
+     *
+     */
+    public static function compareVersion($version)
+    {
+        return version_compare($version, self::VERSION);
+    }
 }
