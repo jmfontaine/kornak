76bc18ed24526a9a461955d9829486a3ab91e208
Jean-Marc Fontaine <jm@jmfontaine.net>
2009-10-20 21:36:55 +0000

    Updated Model view helper

--- library/Kornak/View/Helper/Model.php
+++ library/Kornak/View/Helper/Model.php
@@ -14,19 +14,33 @@
  *
  * @category   Kornak
  * @package    Kornak_View
+ * @subpackage Helper
  * @copyright  Copyright (c) 2008-2009 Kornak Group (http://www.kornak-framework.org/)
  * @version    $Id$
  */
 
-require_once 'Kornak/Loader.php';
+/** @see Zend_Loader */
+require_once 'Zend/Loader.php';
 
+/**
+ * Helper for loading, instantiating and returning a model
+ *
+ * @category   Kornak
+ * @package    Kornak_View
+ * @subpackage Helper
+ * @copyright  Copyright (c) 2008-2009 Kornak Group (http://www.kornak-framework.org/)
+ * @license    http://www.kornak-framework.org/license/new-bsd     New BSD License
+ */
 class Kornak_View_Helper_Model extends Zend_View_Helper_Abstract
 {
-    public function model($name, $module = null)
+	/**
+	 * Loads, instantiate and returns the model
+	 *
+	 * @param string $class Class of the model to return
+	 */
+    public function model($class)
     {
-        Kornak_Loader::loadModel($name, $module);
-
-        $class = $name . 'Model';
+        Zend_Loader::loadClass($class);
         return new $class();
     }
 }
